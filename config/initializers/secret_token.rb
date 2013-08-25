cd78d0610ab55ba9081405d7fea1ccf85ea00799
quantized-data <data1217@gmail.com>
2013-08-25 18:47:09 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Printact::Application.config.secret_token = '0cb7901f1748f6edc016fc42207c6869f4bcaddb1204d366c2a6d9f71c7165d10845a31ccf50021697b8bf65753d66a74223d3f5a320233d6bcc5b162b4271bb'
